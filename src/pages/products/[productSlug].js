d5fdb7b6d1fa445c189b3a80db0d03cb4992b0eb
Ifeoluwa <iadewunm@mercuryholidays.com>
2022-04-28 22:15:42 +0100

    Render products dynamically

---
+++ src/pages/products/[productSlug].js
@@ -0,0 +1,117 @@
+import Head from 'next/head'
+
+import Layout from '@components/Layout';
+import Header from '@components/Header';
+import Container from '@components/Container';
+import Button from '@components/Button';
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql
+} from "@apollo/client";
+
+import styles from '@styles/Product.module.scss'
+
+export default function Product({ product }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{ product.name }</title>
+        <meta name={ product.name } content={`Buy ${product.name} at Jelly Commerce`} />
+      </Head>
+
+      <Container>
+        <div className={styles.productWrapper}>
+          <div className={styles.productImage}>
+            {/* Image */}
+            <img width={product.image.width} height={product.image.height} src={product.image.url} alt="" />
+          </div>
+          <div className={styles.productContent}>
+            <h1>{ product.name }</h1>
+            <div className={styles.productDescription} dangerouslySetInnerHTML={{ 
+              __html: product.description?.html 
+            }}/>
+            <p className={styles.productPrice}>
+              ${ product.price  }
+            </p>
+            <p className={styles.productBuy}>
+              <Button>
+                Add to Cart
+              </Button>
+            </p>
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const client = new ApolloClient({
+    uri: 'https://api-eu-west-2.graphcms.com/v2/cl2ina4w15be201xm7b7u84d5/master',
+    cache: new InMemoryCache()
+  });
+
+  const data = await client.query({
+    query: gql`
+      query PageProduct($slug: String) {
+        product(where: {slug: $slug}) {
+          id
+          image
+          name
+          price
+          description {
+            html
+          }
+        }
+      }
+    `,
+    variables: {
+      slug: params.productSlug
+    }
+  })
+
+  const product = data.data.product;
+  console.log('Product', product );
+
+  return {
+    props: {
+      product
+    }
+  }
+}
+
+export async function  getStaticPaths() {
+  const client = new ApolloClient({
+    uri: 'https://api-eu-west-2.graphcms.com/v2/cl2ina4w15be201xm7b7u84d5/master',
+    cache: new InMemoryCache()
+  });
+
+  const data = await client.query({
+    query: gql`
+      query PageProducts {
+        products {
+          name
+          price
+          slug
+          image
+        }
+      }
+    `
+  })
+
+  const paths = data.data.products.map(product => {
+    return {
+      params: {
+        productSlug: product.slug
+      }
+    }
+  })
+
+  console.log(paths);
+
+  return {
+    paths,
+    fallback: false
+  }
+}
